44f91180b8c62a75df26fec401704f1dc5a2f390
Justin <justin@nodesocket.com>
2023-08-26 18:10:42 -0500

    remove battery voltage and current

--- software/battery/database-schema.sql
+++ software/battery/database-schema.sql
@@ -4,8 +4,6 @@ CREATE TABLE battery (
     id uuid DEFAULT gen_random_uuid(),
     percent NUMERIC(4, 1) NOT NULL,
     temperature SMALLINT NOT NULL,
-    voltage NUMERIC(4, 2) NOT NULL,
-    current NUMERIC(4, 2) NOT NULL,
     added TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
     PRIMARY KEY (id)
