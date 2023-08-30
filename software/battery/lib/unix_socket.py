15cd528c027a164ccfed734b43326339ad76231f
Justin <justin@nodesocket.com>
2023-08-29 20:15:20 -0500

    updated unix socket

--- software/battery/lib/unix_socket.py
+++ software/battery/lib/unix_socket.py
@@ -40,7 +40,10 @@ def send(_socket_client, _command, receive_buffer=512):
 
 
 def close(_socket_client):
-    if _socket_client and is_socket(_socket_client):
-        socket_client_path = _socket_client.getpeername()
-        _socket_client.close()
-        log.debug(f"closed Unix socket connection to '{socket_client_path}'")
+    if _socket_client:
+        try:
+            socket_client_path = _socket_client.getpeername()
+            _socket_client.close()
+            log.debug(f"closed Unix socket connection to '{socket_client_path}'")
+        except socket.error as err:
+            pass
