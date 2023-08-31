c324b7a25772d0557c1842f3750f6a9f281d5503
Justin <justin@nodesocket.com>
2023-08-30 20:22:22 -0500

    fix install-service.sh

--- software/battery/install-service.sh
+++ software/battery/install-service.sh
@@ -6,7 +6,7 @@ Description=StratoPi Battery
 After=network.target
 
 [Service]
-ExecStart=app.py
+ExecStart=/home/pi/stratopi/software/battery/app.py
 WorkingDirectory=/home/pi/stratopi/software/battery
 Restart=always
 User=root
