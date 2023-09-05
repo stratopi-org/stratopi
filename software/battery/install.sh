fce98065608a06a545ba8a5fe27cc9ff8611a897
Justin <justin@nodesocket.com>
2023-09-05 18:33:43 -0500

    updated battery install script

--- software/battery/install.sh
+++ software/battery/install.sh
@@ -3,6 +3,7 @@ set -eo pipefail; [[ $TRACE ]] && set -x
 
 # install pip packages globally
 sudo pip install --quiet -r requirements.txt
+echo "✅ Installed pip packages"
 
 cat << EOF | sudo tee /etc/systemd/system/stratopi-battery.service > /dev/null
 [Unit]
@@ -21,10 +22,11 @@ EnvironmentFile=/etc/environment
 [Install]
 WantedBy=multi-user.target
 EOF
+echo "✅ Created systemd service 'stratopi-battery.service'"
 
 sudo systemctl daemon-reload
 echo "✅ Reloaded systemctl daemon"
 sudo systemctl enable stratopi-battery.service
-echo "✅ Enabled service 'stratopi-battery.service'"
+echo "✅ Enabled systemd service 'stratopi-battery.service'"
 sudo systemctl restart stratopi-battery.service
-echo "✅ Restarted service 'stratopi-battery.service'"
+echo "✅ Restarted systemd service 'stratopi-battery.service'"
