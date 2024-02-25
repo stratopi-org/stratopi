5ae60e10501e23c69b51c0eeea63c96a95abddfc
Justin <justin@nodesocket.com>
2024-02-25 00:02:23 -0600

    remove expression index in location

--- software/location/database-schema.sql
+++ software/location/database-schema.sql
@@ -26,9 +26,6 @@ CREATE INDEX location_date_idx
 CREATE INDEX location_time_idx
     ON location (time ASC);
 
-CREATE INDEX location_timestamp_idx
-    ON location ((date + time)::TIMESTAMP);
-
 CREATE INDEX location_coordinates_idx
     ON location USING GIST(coordinates);
 
