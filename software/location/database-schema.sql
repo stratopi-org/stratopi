9f5dc674148122727da6ee2c78e69160563ce8c7
Justin <justin@nodesocket.com>
2024-02-26 22:29:09 -0600

    slight update to location database schema

--- software/location/database-schema.sql
+++ software/location/database-schema.sql
@@ -30,10 +30,10 @@ CREATE INDEX location_coordinates_idx
     ON location USING GIST(coordinates);
 
 CREATE INDEX location_altitude_m_idx
-    ON location (altitude_m);
+    ON location (altitude_m ASC);
 
 CREATE INDEX location_speed_kn_idx
-    ON location (speed_kn);
+    ON location (speed_kn ASC);
 
 CREATE INDEX location_added_idx
     ON location (added ASC);
