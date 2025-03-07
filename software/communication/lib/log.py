2eaf6164635d65ef54edaaef1d4c95b659ae82dc
Justin <justin@nodesocket.com>
2025-03-07 02:24:08 -0600

    remove Z (utc) from log lib since uses local time

--- software/communication/lib/log.py
+++ software/communication/lib/log.py
@@ -4,14 +4,14 @@ import sys
 
 log_level = os.getenv('LOG_LEVEL', 'INFO').upper()
 logging.basicConfig(level=log_level,
-                    format='%(asctime)s.%(msecs)03dZ [%(levelname)s] %(message)s',
+                    format='%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s',
                     datefmt='%Y-%m-%dT%H:%M:%S',
                     force=True)
 
 stdout_logging = logging.Logger(name='stdout_logging',
                                 level=log_level)
 stdout_stream = logging.StreamHandler(stream=sys.stdout)
-stdout_stream.setFormatter(logging.Formatter('%(asctime)s.%(msecs)03dZ [%(levelname)s] %(message)s',
+stdout_stream.setFormatter(logging.Formatter('%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s',
                                              datefmt='%Y-%m-%dT%H:%M:%S'))
 stdout_logging.addHandler(stdout_stream)
 
